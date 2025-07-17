75f4ba03f2e3bb856b955ed086786594961654b3
Teemu Miettinen <me@example.com>
2025-07-17 15:01:28 +0300

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,19 @@
 // using System;
-
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public bool jumpKeyWasPressed;
+    public int moveSpeed;
+    
     public float horizontalInput;
     public Rigidbody rigidbodyComponent;
     public Transform groundCheckTransform;
     [SerializeField] private LayerMask playerMask;
+
+    public int superJumpsRemaining;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,27 +27,50 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             jumpKeyWasPressed = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene("StartScene");
+        }
         
         horizontalInput = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        rigidbodyComponent.linearVelocity = new Vector3(horizontalInput, rigidbodyComponent.linearVelocity.y, 0);
+        rigidbodyComponent.linearVelocity = new Vector3(horizontalInput * moveSpeed, rigidbodyComponent.linearVelocity.y, 0);
+        // Debug.Log(rigidbodyComponent.linearVelocity.y);
             
         if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) {
             return;
         }
             
         
-        if (jumpKeyWasPressed) 
-        {
-            rigidbodyComponent.AddForce(Vector3.up * 7, ForceMode.VelocityChange);
+        if (jumpKeyWasPressed) {
+            
+            float jumpPower = 5;
+            if (superJumpsRemaining > 0) {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
             jumpKeyWasPressed = false;
         }
         
         
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9) 
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+        
+        if (other.gameObject.layer == 10) 
+        {
+            Destroy(other.gameObject);
+            SceneManager.LoadScene("StartScene");
+        }
+    }
 }
